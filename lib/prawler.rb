7b6a6d8490f348a8ff71111c160fa37cc69c1b97
Dan Jakob Ofer <dan@ofer.to>
2017-03-31 22:14:17 -0400

    wip

--- lib/prawler.rb
+++ lib/prawler.rb
@@ -8,8 +8,9 @@ require 'active_support/logger'
 require 'active_support/core_ext'
 
 require 'prawler/version'
-require 'prawler/application'
 require 'prawler/questionnaire'
 
 require 'prawler/git/cli'
 require 'prawler/git/config'
+
+require 'prawler/application'
